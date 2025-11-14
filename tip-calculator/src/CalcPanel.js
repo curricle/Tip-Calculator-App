0f48fcd7c9280e20fb8b75f45fd1a522391f6aec
S. Jordan <curricle@gmail.com>
2025-11-14 15:01:31 -0500

    Add static panels to app

---
+++ tip-calculator/src/CalcPanel.js
@@ -0,0 +1,42 @@
+function NumInput({ title, amount }) {
+    return(
+        <div className="Num-Input-Container">
+            <h3>{title}</h3>
+            <input></input>
+        </div>
+    );
+}
+
+function Button({ amount, canBeSet }) {
+    return(
+        <button>{ canBeSet ? canBeSet : amount }</button>
+    );
+}
+
+function TipButtons() {
+    return(
+        <div className="Tip-Buttons">
+            <h3>Select Tip %</h3>
+            <div className="Buttons-List">
+                <Button amount="5%" />
+                <Button amount="10%" />
+                <Button amount="15%" />
+                <Button amount="20%" />
+                <Button amount="50%" />
+                <Button amount="Custom" canBeSet="Custom" />
+            </div>
+        </div>
+    );
+}
+
+function CalcPanel() {
+    return(
+        <div className="Calc-Panel">
+            <NumInput title="Bill" amount="142.55" />
+            <TipButtons />
+            <NumInput title="Number of People" amount="5" />
+        </div>
+    );
+}
+
+export default CalcPanel;
---
+++ tip-calculator/src/DisplayPanel.js
@@ -0,0 +1,24 @@
+function AmountDisplay({ title, amount }) {
+    return(
+        <div className="Amount-Display">
+            <div className="Title">
+                <h3>{title}</h3>
+            <br />/ person
+            </div>
+            <div className="Amount">
+                {amount}
+            </div>
+        </div>
+    );
+}
+
+function DisplayPanel() {
+  return (
+    <div className="Display-Panel">
+        <AmountDisplay title="Tip Amount" amount="$4.29" />
+        <AmountDisplay title="Total" amount="$32.79" />
+    </div>
+  );  
+}
+
+export default DisplayPanel;
