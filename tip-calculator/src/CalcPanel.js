b87417f6ddc5162351e21ab60ff994a65576c3b7
S. Jordan <curricle@gmail.com>
2025-11-14 20:53:13 -0500

    Add required input patterns and validity styling

--- tip-calculator/src/CalcPanel.js
+++ tip-calculator/src/CalcPanel.js
@@ -2,18 +2,50 @@ import './CalcPanel.css';
 import dollar from './images/icon-dollar.svg';
 import person from './images/icon-person.svg'
 
-function NumInput({ title, amount, icon }) {
+function NumInput({ title, amount, icon, inputId }) {
     const inputIconBackground = {
         backgroundImage: `url(${icon})`,
         backgroundRepeat: 'no-repeat',
         backgroundPosition: '16px center',
     };
 
+    function setInputPattern(inputId) {
+        if(inputId == "Bill-Total") {
+            return '^\\d+\\.\\d{2}$';
+        }
+        else if(inputId == "Party-Size") {
+            return '[1-9]+';
+        }
+    }
+
+    function setInputMode(inputId) {
+        if(inputId == "Bill-Total") {
+            return "decimal";
+        }
+        else {
+            return "numeric";
+        }
+    }
+
+    function checkInputValidity(inputContent) {
+        if(ValidityState) {
+            return;
+        }
+        else {
+            return;
+        }
+    }
+
     return(
         <div className="Num-Input-Container">
             <h3>{title}</h3>
             <div className="Num-Input">
-                <input type="text" style={inputIconBackground} />
+                <input 
+                    inputmode={setInputMode(inputId)} 
+                    pattern={setInputPattern(inputId)}
+                    placeholder="0"  
+                    style={inputIconBackground} 
+                    />
             </div>
         </div>
     );
@@ -44,9 +76,9 @@ function TipButtons() {
 function CalcPanel() {
     return(
         <div className="Calc-Panel">
-            <NumInput title="Bill" amount="142.55" icon={dollar} />
+            <NumInput title="Bill" amount="142.55" icon={dollar} inputId="Bill-Total" />
             <TipButtons />
-            <NumInput title="Number of People" amount="5" icon={person} />
+            <NumInput title="Number of People" amount="5" icon={person} inputId="Party-Size" />
         </div>
     );
 }
